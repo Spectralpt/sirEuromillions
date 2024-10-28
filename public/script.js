8f33aca249ba324da4601b4c26706f4e406f8e8c
Miguel <miguelfranquiralves@gmail.com>
2024-10-28 17:05:08 +0000

    updated number and star showcase

--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
 async function getNumbers() {
-    const numbers = document.getElementById("numbers")
-    const stars = document.getElementById("stars")
+    //const numbers = document.getElementById("numbers")
+    //const stars = document.getElementById("stars")
+
+    const numbers = document.getElementsByClassName("numbers")
+    const stars = document.getElementsByClassName("stars")
 
     const res = await fetch("/euro")
 
     let data = await res.json()
 
-    numbers.innerText = data["key"]["numbers"]
-    stars.innerText = data["key"]["stars"]
+    //numbers.innerText = data["key"]["numbers"]
+    //stars.innerText = data["key"]["stars"]
+
+    const numbersArray = data["key"]["numbers"]
+    Array.from(numbers).forEach((element, index) => {
+        element.innerText = numbersArray[index]
+    })
+
+    const starsArray = data["key"]["numbers"]
+    Array.from(stars).forEach((element, index) => {
+        element.innerText = starsArray[index]
+    })
 }
 
